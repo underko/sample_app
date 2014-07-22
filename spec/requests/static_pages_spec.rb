6e5721bbec366243b02e258352f4eaab31af644b
underko <martin.janocko.gih@gmail.com>
2014-07-22 11:00:16 +0200

    Static pages html finished

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,6 +14,11 @@ describe "Static pages" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
 		end
+
+		it "should have the right title" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+		end
 	end
 
 	describe "Help page" do
@@ -21,6 +26,11 @@ describe "Static pages" do
 		it "should have the content 'Help'" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
 		end
 	end
 
@@ -30,6 +40,11 @@ describe "Static pages" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
 		end
+
+		it "should have the right title" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+		end
 	end  
-	
+
 end
